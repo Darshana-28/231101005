7c0828cfe697958157307bade18173f8c8d895f9
Darshana-28 <vadneredarshana7128@gmail.com>
2026-06-02 10:00:29 +0530

    Add Selenium test to open Google and print title

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    @Test
+    public void openGoogle() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
